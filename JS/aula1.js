a2c0540f76eb2bb5e93a548e7caf6cf080b3d437
FaveroNath <faveronathalia@gmail.com>
2021-01-10 20:07:36 -0300

    Added h1 tag

--- JS/aula1.js
+++ JS/aula1.js
@@ -1,4 +1,51 @@
 
+//strings
+/*
 console.log(`
 	Bem vindos ao "Starter" 
 	'hello world'`);
+console.log(`$(1+1)`);
+// ""
+//''
+//``
+*/
+//console.log(undefined === null);
+//objeto
+/*console.log({
+	nome: "Nathalia",
+	idade: 19,
+	andar: function(){
+		console.log('andar')
+	}
+})*/
+//array
+//console.log(["Nathalia","cachorro","gato","papagaio","queijo"])
+
+// var const let
+//const clima = "quente"
+
+//clima = "Frio";
+//console.log(clima)
+
+//let clima = 0
+//clima = true;
+//console.log(typeof clima)
+/*
+console.log('>existe x depois do bloco?',x)
+{
+	var x = 0;
+}
+console.log('>existe x depois do bloco?',x)
+*/
+
+/*let y = 1
+{
+	y = 0;
+	console.log('>existe y depois do bloco?',y)
+
+}
+console.log('>existe y depois do bloco?',y)
+*/
+let name = "Nath"
+let checkName = name === "Nath";
+checkName
